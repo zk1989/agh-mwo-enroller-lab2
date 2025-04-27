4731570039b253a9e22f84f526eee82b80ae8e3e
zkocik <zkocik@d17-431-pc-20.ipa.ii.agh.edu.pl>
2025-04-27 13:52:40 +0200

    Add Meetings

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,31 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.persistence.MeetingService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+    @Autowired
+    MeetingService meetingService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetings() {
+        Collection<Meeting> meetings = meetingService.getAll();
+        return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+}
